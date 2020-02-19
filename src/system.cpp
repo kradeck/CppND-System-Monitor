5a7595177fa223dcfa766cb2a75e1447d9d0bc60
Krzysztof Radecki <radeckikrzysztof@gmail.com>
2020-02-19 10:39:03 -0800

    returned os name

--- src/system.cpp
+++ src/system.cpp
@@ -7,11 +7,21 @@
 #include "process.h"
 #include "processor.h"
 #include "system.h"
+#include "linux_parser.h"
 
 using std::set;
 using std::size_t;
 using std::string;
 using std::vector;
+
+
+System::System() :
+    operating_system_{LinuxParser::OperatingSystem()}
+{
+    // The parameters loaded in the ctor are constant, and rather
+    // they won't change during the program execution (like the OS name).
+    // Summarizing we want to read them once.
+}
 
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
@@ -25,8 +35,8 @@ std::string System::Kernel() { return string(); }
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// DONE: Return the operating system name
+std::string System::OperatingSystem() { return operating_system_; }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
